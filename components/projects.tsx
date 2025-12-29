786f78b2ab29c969e8c813fa0d46f83dcad46b06
Owen Vassarotto <ovassarotto@familiazuccardi.com>
2025-12-28 23:47:49 -0300

    Updated profile image and added maintaining info in the projects section

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,24 +1,24 @@
 "use client";
 
-import SectionHeading from './section-heading'
-import { projectsData } from '@/lib/data'
-import Project from './project'
-import React from 'react'
-import { useSectionInView } from '@/lib/hooks';
+import SectionHeading from "./section-heading";
+import { projectsData } from "@/lib/data";
+import Project from "./project";
+import React from "react";
+import { useSectionInView } from "@/lib/hooks";
 
 export default function Projects() {
-
   const { ref } = useSectionInView("Proyectos", 0.5);
 
   return (
-    <section
-        ref={ref}
-        id='projects'
-        className='scroll-mt-28 mb-28'
-    >
-        <SectionHeading>Mis Proyectos</SectionHeading>
+    <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
+      <SectionHeading>Mis proyectos</SectionHeading>
 
-        <div>
+      <div className="flex items-center gap-4 mt-16">
+        <div className="h-5 w-5 animate-spin rounded-full border-2 border-gray-300 border-t-gray-800"></div>
+        <p>Contenido en proceso de actualización</p>
+      </div>
+
+      {/* <div>
             {projectsData.map((project, index) => (
                 <React.Fragment key={index}>
                     <Project 
@@ -26,8 +26,7 @@ export default function Projects() {
                     />
                 </React.Fragment>
             ))}
-        </div>
+        </div> */}
     </section>
-  )
+  );
 }
-
--- components/section-heading.tsx
+++ components/section-heading.tsx
@@ -6,6 +6,6 @@ type SectionHeadingProps = {
 
 export default function SectionHeading({ children } : SectionHeadingProps) {
   return (
-    <h2 className="font-medium text-3xl capitalize mb-8 text-center">{children}</h2>
+    <h2 className="font-medium text-3xl mb-8 text-center">{children}</h2>
   )
 }
